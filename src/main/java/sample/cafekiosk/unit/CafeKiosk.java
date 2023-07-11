de88e9029880b6f5592effb7ba78b6910402c21e
kmg0485 <kmg0485@naver.com>
2023-07-11 14:13:05 +0900

    기초설정

--- src/main/java/sample/cafekiosk/unit/CafeKiosk.java
+++ src/main/java/sample/cafekiosk/unit/CafeKiosk.java
@@ -1,6 +1,37 @@
 package sample.cafekiosk.unit;
 
+import sample.cafekiosk.unit.beverage.Beverage;
+import sample.cafekiosk.unit.order.Order;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 public class CafeKiosk {
-    public void add(Americano americano) {
+
+    private List<Beverage> beverages = new ArrayList<>();
+
+    public void add(Beverage beverage) {
+        beverages.add(beverage);
+    }
+
+    public void remove(Beverage beverage){
+        beverages.remove(beverage);
+    }
+
+    public void clear(Beverage beverage){
+        beverages.clear();
+    }
+
+    public int calculateTotalPrice() {
+        int totalPrice = 0;
+        for (Beverage beverage : beverages) {
+            totalPrice += beverage.getPrice();
+        }
+        return totalPrice;
+    }
+
+    public Order createOrder() {
+        return new Order(LocalDateTime.now(), beverages);
     }
 }
